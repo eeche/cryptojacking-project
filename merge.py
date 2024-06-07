d66a4ea7b8e61e9273fa854f98d29498eeb2a8a4
eeche <dlckdgus200011@gmail.com>
2024-06-08 02:49:51 +0900

    feat: add features_based_count.csv

--- merge.py
+++ merge.py
@@ -2,9 +2,9 @@ import csv
 import pandas as pd
 
 df = pd.read_csv(
-    'C:\\Users\\dlckd\\Desktop\\cryptoProject\\random_forest\\new_frequency_features_corrected.csv')
+    'C:\\Users\\dlckd\\Desktop\\cryptoProject\\Data\\features_based_time.csv')
 add = pd.read_csv(
-    'C:\\Users\\dlckd\\Desktop\\cryptoProject\\features_count.csv')
+    'C:\\Users\\dlckd\\Desktop\\cryptoProject\\Data\\features_based_count.csv')
 
 merged_dataframe = pd.concat([df, add])
 
