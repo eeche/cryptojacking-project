1184c107d8bfc9e488962a8043c0aa27bf43a81f
eeche <dlckdgus200011@gmail.com>
2024-06-07 22:31:29 +0900

    fix: detect_syscall.sh 수정

--- actual_running_program/detect_syscall.sh
+++ actual_running_program/detect_syscall.sh
@@ -4,7 +4,7 @@
 CURRENT_TIME=$(date "+%Y%m%d_%H%M%S")
 LOG_DIR="/tmp/cjcheck"
 LOG_FILE="${LOG_DIR}/${CURRENT_TIME}.txt"
-FREQUENCY_FILE="${LOG_FILE%.txt}_frequency.txt"
+FREQUENCY_FILE="${LOG_FILE}_frequency.txt"
 MODEL_FILE="random_forest_model.pkl"
 SCALER_FILE="scaler.pkl"
 PYTHON_SCRIPT="/detect_syscall.py"
@@ -13,10 +13,15 @@ PYTHON_SCRIPT="/detect_syscall.py"
 mkdir -p $LOG_DIR
 
 # sudo 권한으로 trace-cmd 실행 (5분간)
-sudo trace-cmd record -e syscall -o /tmp/trace.dat & sleep 300; sudo pkill trace-cmd
+sudo trace-cmd record -e syscall &
+TRACE_CMD_PID=$!
+
+sleep 300
+
+sudo kill -SIGINT $TRACE_CMD_PID
 
 # trace-cmd 보고서 생성
-sudo trace-cmd report /tmp/trace.dat > $LOG_FILE
+sudo trace-cmd report > $LOG_FILE
 
 # Python 스크립트 실행
 python3 $PYTHON_SCRIPT $LOG_FILE $MODEL_FILE $SCALER_FILE
