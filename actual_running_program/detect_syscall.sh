8f3a15e6b7ad510729d2c33b3dc79cb268c8c6c3
dangdang1ing15 <shine_0512@naver.com>
2024-06-07 22:52:45 +0900

    fix : fix detect script path error

--- actual_running_program/detect_syscall.sh
+++ actual_running_program/detect_syscall.sh
@@ -2,26 +2,26 @@
 
 # 현재 시간 형식 설정
 CURRENT_TIME=$(date "+%Y%m%d_%H%M%S")
-LOG_DIR="/tmp/cjcheck"
-LOG_FILE="${LOG_DIR}/${CURRENT_TIME}.txt"
-FREQUENCY_FILE="${LOG_FILE}_frequency.txt"
-MODEL_FILE="random_forest_model.pkl"
-SCALER_FILE="scaler.pkl"
-PYTHON_SCRIPT="/detect_syscall.py"
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+LOG_FILE="${SCRIPT_DIR}/${CURRENT_TIME}.txt"
+FREQUENCY_FILE="${LOG_FILE%.txt}_frequency.txt"
+MODEL_FILE="${SCRIPT_DIR}/voting_classifier_model.pkl"
+SCALER_FILE="${SCRIPT_DIR}/scaler.pkl"
+PYTHON_SCRIPT="${SCRIPT_DIR}/detect_syscall.py"
+TRACE_CMD="/usr/bin/trace-cmd"  # trace-cmd의 절대 경로
 
-# 디렉토리 생성
-mkdir -p $LOG_DIR
+# 디렉토리 생성 (현재 디렉토리 사용)
+mkdir -p $SCRIPT_DIR
 
-# sudo 권한으로 trace-cmd 실행 (5분간)
-sudo trace-cmd record -e syscall &
-TRACE_CMD_PID=$!
+# trace-cmd 실행 (5분간)
+sudo $TRACE_CMD record -e syscalls -o "${SCRIPT_DIR}/trace.dat" & TRACE_CMD_PID=$!
 
 sleep 300
 
 sudo kill -SIGINT $TRACE_CMD_PID
 
 # trace-cmd 보고서 생성
-sudo trace-cmd report > $LOG_FILE
+sudo $TRACE_CMD report "${SCRIPT_DIR}/trace.dat" > $LOG_FILE
 
 # Python 스크립트 실행
-python3 $PYTHON_SCRIPT $LOG_FILE $MODEL_FILE $SCALER_FILE
+sudo -H python3 $PYTHON_SCRIPT $LOG_FILE $MODEL_FILE $SCALER_FILE $FREQUENCY_FILE
