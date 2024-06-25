fa69eb3330b43f38d945172e53771ec6a663c868
eeche <dlckdgus200011@gmail.com>
2024-06-25 18:11:10 +0900

    fix: fix Service_lists shell scripts
    change route not to save crptomining data at local PC

--- Service_lists/django-project/syscall_capture.sh
+++ Service_lists/django-project/syscall_capture.sh
@@ -34,7 +34,7 @@ done
 
 docker run -d \
     -e TZ=Europe/Berlin \
-    -v ~/.bytecoin:/root/.bytecoin \
+    -v /root/bytecoin \
     --restart unless-stopped \
     -p 8080:8080 \
     -p 8081:8081 \
