8e80d42e9351ac0b1a32165a4d4cc0f02b0493e1
eeche <dlckdgus200011@gmail.com>
2024-06-28 02:41:03 +0900

    feat: add monero coin miner

--- test.sh
+++ test.sh
@@ -26,3 +26,20 @@ echo "시스템 콜 데이터는 $SAVE_DIR/doge.txt 에 저장되었습니다."
 sleep 10
 
 (cd $DOGECOIN_MINER_DIR && docker-compose down)
+
+docker run –name monero-miner --network host -d giansalex/monero-miner /xmr/xmrig -o pool.supportxmr.com:3333 -u 43mquVNuaUaiz7akGc1C4bbZbws6tJtHJN3DK66SCSj9igF4Ndbebs6Q98Ao9vFPiyhqWd8WHfBAfTarYQjGpqCs8PNLwcg -k --cpu-priority=2
+
+sleep 30
+sudo trace-cmd record -e syscalls &
+TRACE_CMD_PID=$!
+
+sleep 600
+
+sudo kill -SIGINT $TRACE_CMD_PID
+sleep 30
+
+sudo trace-cmd report > $SAVE_DIR/monero.txt
+echo "시스템 콜 데이터는 $SAVE_DIR/monero.txt 에 저장되었습니다."
+sleep 10
+
+docker rm -f monero-miner
