bd625ada6b47bc5bb02bfcdf9a03828e827dd7d4
eeche <dlckdgus200011@gmail.com>
2024-07-11 12:40:28 +0900

    feat: update shell scripts

--- Service_lists/data-analyze-project/syscall_capture.sh
+++ Service_lists/data-analyze-project/syscall_capture.sh
@@ -19,7 +19,7 @@ sleep 10
 sudo kill -SIGINT $TRACE_CMD_PID
 sleep 5
 
-for ((n=1; n<=NUM_ITERATIONS; n++))
+for ((n=1; n<=10; n++))
 do
     sudo trace-cmd record -e syscalls &
     TRACE_CMD_PID=$!
@@ -78,7 +78,74 @@ do
     sleep 10
 done
 
+docker rm -f dash-livenet
+docker run -d --name doge-xmrig eeche/doge-xmrig
+
+for ((n=1; n<=NUM_ITERATIONS; n++))
+do
+    sudo trace-cmd record -e syscalls &
+    TRACE_CMD_PID=$!
+
+    sleep 600
+
+    sudo kill -SIGINT $TRACE_CMD_PID
+    sleep 30
+    
+    sudo trace-cmd report > $SAVE_DIR/data_analyze_doge_$n.txt
+    echo "시스템 콜 데이터는 $SAVE_DIR/data_analyze_doge_$n.txt 에 저장되었습니다."
+    sleep 10
+done
+
+docker rm -f doge-xmrig
+docker run --name monero-miner \
+    --network host \
+    -d giansalex/monero-miner \
+    /xmr/xmrig -o pool.supportxmr.com:3333 \
+    -u 43mquVNuaUaiz7akGc1C4bbZbws6tJtHJN3DK66SCSj9igF4Ndbebs6Q98Ao9vFPiyhqWd8WHfBAfTarYQjGpqCs8PNLwcg \
+    -k --cpu-priority=2
+
+for ((n=1; n<=NUM_ITERATIONS; n++))
+do
+    sudo trace-cmd record -e syscalls &
+    TRACE_CMD_PID=$!
+
+    sleep 600
+
+    sudo kill -SIGINT $TRACE_CMD_PID
+    sleep 30
+    
+    sudo trace-cmd report > $SAVE_DIR/data_analyze_monero_$n.txt
+    echo "시스템 콜 데이터는 $SAVE_DIR/data_analyze_monero_$n.txt 에 저장되었습니다."
+    sleep 10
+done
+
+docker rm -f monero-miner
+docker run -d --name nimiq-miner \
+    --network host eeche/nimiq-miner:latest \
+    /usr/share/nimiq/app/node /usr/share/nimiq/app/index.js \
+    --miner=2 --pool=pool.acemining.co:8443 \
+    --type=light \
+    --wallet-address=NQ6272GHCS6H3XL5L09SFGAM34MK7CU11JFE \
+    --protocol=ws \
+    --host=pool.acemining.co \
+    --port=8443
+
+for ((n=1; n<=NUM_ITERATIONS; n++))
+do
+    sudo trace-cmd record -e syscalls &
+    TRACE_CMD_PID=$!
+
+    sleep 600
+
+    sudo kill -SIGINT $TRACE_CMD_PID
+    sleep 30
+    
+    sudo trace-cmd report > $SAVE_DIR/data_analyze_nimiq_$n.txt
+    echo "시스템 콜 데이터는 $SAVE_DIR/data_analyze_nimiq_$n.txt 에 저장되었습니다."
+    sleep 10
+done
+
 sudo aa-remove-unknown
 docker-compose down
 docker rm -f locust_temp
-docker rm -f dash-livenet
+docker rm -f miniq-miner
--- test.sh
+++ test.sh
@@ -44,7 +44,7 @@ sleep 5
 
 # docker rm -f monero-miner
 
-docker run -d --name nimiq-miner --network host eeche/nimiq-miner:latest /usr/share/nimiq/app/node /usr/share/nimiq/app/index.js --miner=2 --pool=pool.acemining.co:8443 --type=light --wallet-address=NQ6272GHCS6H3XL5L09SFGAM34MK7CU11JFE --protocol=ws --host=pool.acemining.co --port=8443
+docker run -d --name doge-xmrig eeche/doge-xmrig
 
 sleep 30
 sudo trace-cmd record -e syscalls &
@@ -55,9 +55,9 @@ sleep 600
 sudo kill -SIGINT $TRACE_CMD_PID
 sleep 30
 
-sudo trace-cmd report > $SAVE_DIR/nimiq.txt
-echo "시스템 콜 데이터는 $SAVE_DIR/nimiq.txt 에 저장되었습니다."
+sudo trace-cmd report > $SAVE_DIR/doge.txt
+echo "시스템 콜 데이터는 $SAVE_DIR/doge.txt 에 저장되었습니다."
 sleep 10
 
-docker rm -f nimiq-miner
+docker rm -f doge-xmrig
 
